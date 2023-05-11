72ed15ab7c04656a18b4408acf6d4339ef090e67
zeotoni <zeotoni666@gmail.com>
2023-05-10 22:42:48 -0300

    pagina de cadastro desktop

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header/header.component';
+import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
 import { MsgComponent } from './msg-warning/msg-warning.component';
 
